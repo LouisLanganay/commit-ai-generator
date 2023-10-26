50ecc12c17ada4747b4b4e6a10ff4504f3d51703
LouisLanganay <louis.langanay@epitech.eu>
2023-10-26 21:53:40 +0200

    fix: fix typo in error message

--- src/utils/getError.ts
+++ src/utils/getError.ts
@@ -3,7 +3,7 @@ const getError = () => ({
   no_prompt: 'Commit norm not found or no custom prompt set.',
   no_api_key: 'OpenAI API Key is not set. Please set it in the settings.',
   api_key_set: 'OpenAI API Key set successfully.',
-  already_running: 'Commit AI is already isRunning. Please wait until it is finished.',
+  already_running: 'Commit AI is already running. Please wait until it is finished.',
   no_git: 'Git extension not found. Please install it and try again.',
   running: 'Generating commit message...',
   no_changes: 'No changes to commit.',
